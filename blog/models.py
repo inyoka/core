dd14d64617ee5f23c3dc95a82c5c4ad1dc358f03
Simon <simonmarkholland@gmail.com>
2018-07-24 15:52:39 +0700

    Add Aside to index page.

--- blog/models.py
+++ blog/models.py
@@ -13,4 +13,13 @@ class BlogIndexPage(Page):
         ('image', ImageChooserBlock(null=True, blank=True)),
     ], null=True, blank=True)
 
-    content_panels = Page.content_panels + [ StreamFieldPanel('intro') ]
+    aside = StreamField([
+        ('heading', blocks.CharBlock(classname="full title", null=True, blank=True)),
+        ('paragraph', blocks.RichTextBlock(null=True, blank=True)),
+        ('image', ImageChooserBlock(null=True, blank=True)),
+    ], null=True, blank=True)
+
+    content_panels = Page.content_panels + [
+        StreamFieldPanel('intro'),
+        StreamFieldPanel('aside')
+    ]
