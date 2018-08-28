dfd10297a7546f0b9e8e94e40359d3e50c80963c
Simon <simonmarkholland@gmail.com>
2018-08-28 09:22:58 +0700

    Add a contact page model

--- home/models.py
+++ home/models.py
@@ -1,10 +1,17 @@
+from .blocks import *
+
 from django.db import models
 
 from wagtail.core.models import Page
 from wagtail.core.fields import StreamField
+
 from wagtail.core import blocks
-from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel
 from wagtail.images.blocks import ImageChooserBlock
+from wagtail.embeds.blocks import EmbedBlock
+
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel, \
+        FieldRowPanel, MultiFieldPanel, InlinePanel, PageChooserPanel
+
 
 class HomePage(Page):
     body = StreamField([
@@ -20,3 +27,14 @@ class HomePage(Page):
     ], null=True, blank=True)
 
     content_panels = Page.content_panels + [ StreamFieldPanel('body'), StreamFieldPanel('aside') ]
+
+
+class ContactPage(Page):
+    page_content = StreamField([
+        ('heading', blocks.CharBlock(classname="full title",icon="title")),
+        ('paragraph', blocks.RichTextBlock()),
+        ('image', ImageChooserBlock(icon="image")),
+        ('two_columns', TwoColumnBlock()),
+        ('embedded_video', EmbedBlock(icon="media")),
+        ('google_map', GoogleMapBlock()),
+    ],null=True,blank=True)
